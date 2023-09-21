c3cb71a6756ff81bb1518c216a484cd7bd82f82e
NGregorczyk <natalia.gregorczyk002@gmail.com>
2023-09-21 15:10:20 +0200

    uwu

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,34 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("wpisz rozmiar tablicy");
+        int sizeTab = scanner.nextInt();
+        int[] numberTab = new int[sizeTab];
+        for (int i = 0; 1 < sizeTab; i++) {
+
+            if (i % 10 == 0) {
+                System.out.println();
+            }
+            numberTab[i] = (int) (Math.random() * 100);
+            System.out.print(numberTab[i] + ", ");
 
         }
+        System.out.println("wpisz jakaliczb echcesz odszukac");
+        int lockedNumber = scanner.nextInt();
+    }
+
+
+    public boolean findNumber(int[] numberTab, int lockedForNumber) {
+        int i = 0;
+        int size = numberTab.length;
+        while (i < size && numberTab[i] != lockedForNumber) {
+            i++;
+        }
+        if(i==size)return false;
+        return true;
+
+    };
     }
